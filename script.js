72c2e2232b228bd46ff816e648571502e388b898
LAMKIRICH AHMED <ahmed.lamkirich1999@gmail.com>
2025-06-05 00:06:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,20 +5,15 @@ const quotes = [
   "JavaScript is fun!",
   "Let’s build cool things together!"
 ];
-
 const quote = document.querySelector('.quote');
 let index = 0;
-
 function showQuote() {
   quote.style.opacity = 0;
-
   setTimeout(() => {
     quote.textContent = quotes[index];
     quote.style.opacity = 1;
-
     index = (index + 1) % quotes.length;
   }, 300); 
 }
-
 setInterval(showQuote, 2000); 
 showQuote(); 
